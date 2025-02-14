5d2c8176bc3bc1f2c82ed7011877dfb203650a6d
JanuDev <darkober12@gmail.com>
2025-02-14 06:33:04 -0400

    reinit proyect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
